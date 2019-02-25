ce03a6c55abcb869625ede24d60caa592ce6e08c
Aliaksandr Marynovich <aliaksandr_marynovich@epam.com>
2019-02-25 15:42:02 +0300

    Add allow methods

--- app.js
+++ app.js
@@ -73,6 +73,7 @@ app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', req.headers.origin || 'http://localhost');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   next();
 });
 
