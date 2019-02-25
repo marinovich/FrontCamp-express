6d7be28f67f322fa16cce376ec4a3487a6e5eb04
Aliaksandr Marynovich <aliaksandr_marynovich@epam.com>
2019-02-25 06:35:00 +0300

    Fix auth issue

--- app.js
+++ app.js
@@ -70,8 +70,9 @@ app.set('views', './views')
 app.set('view engine', 'pug');
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 
