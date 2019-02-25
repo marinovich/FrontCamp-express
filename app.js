7707290905434c710f3db3a1dbb047ca720984fa
Aliaksandr Marynovich <aliaksandr_marynovich@epam.com>
2019-02-25 08:37:52 +0300

    Replace endpoint /user with /status. Add fix for postman requests

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.set('views', './views')
 app.set('view engine', 'pug');
 
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || 'http://localhost');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header('Access-Control-Allow-Credentials', true);
   next();
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', passport.authenticate('local'), (req, res) => res.send(req.user));
-router.get('/user', (req, res) => res.send(req.user));
+router.get('/status', (req, res) => res.send(req.user ? { isLoggedIn: true } : { isLoggedIn: false }));
 router.get('/logout', (req, res) => {
   req.logout();
   res.send(null);
